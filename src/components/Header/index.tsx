5764cf12da5bb7ebe98b92552cb87217b3eaadb3
Ersoy Efe Uruk <efeuruk1@gmail.com>
2020-06-12 23:15:16 +0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,134 @@
+/* TODO: Küçülme olduğunda hamburger menu eklenecek */
+
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Typography, Box, InputBase, Link } from '@material-ui/core';
+import { createStyles, fade, Theme, makeStyles } from '@material-ui/core/styles';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import SearchIcon from '@material-ui/icons/Search';
+import Badge from '@material-ui/core/Badge';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1
+        },
+        headerContainer: {
+            justifyContent: "space-between"
+        },
+        linkContainer: {
+            display: "flex",
+            justifyContent: "space-between",
+            background: "red"
+        },
+        basketButton: {
+            marginRight: theme.spacing(2),
+        },
+        title: {
+            display: 'none',
+            [theme.breakpoints.up('sm')]: {
+                display: 'block',
+            },
+        },
+        logo: {
+            '&:hover': {
+                textDecoration: 'none'
+            },
+        },
+        search: {
+            position: 'relative',
+            borderRadius: theme.shape.borderRadius,
+            backgroundColor: fade(theme.palette.common.white, 0.15),
+            '&:hover': {
+                backgroundColor: fade(theme.palette.common.white, 0.25),
+            },
+            marginLeft: 0,
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                marginLeft: theme.spacing(1),
+                width: 'auto',
+            },
+        },
+        searchIcon: {
+            padding: theme.spacing(0, 2),
+            height: '100%',
+            position: 'absolute',
+            pointerEvents: 'none',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        inputRoot: {
+            color: 'inherit',
+        },
+        inputInput: {
+            padding: theme.spacing(1, 1, 1, 0),
+            paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                width: '12ch',
+                '&:focus': {
+                    width: '20ch',
+                },
+            },
+        },
+    }),
+);
+
+export const Header: React.FC = () => {
+    const classes = useStyles();
+    const preventDefault = (event: React.SyntheticEvent) => event.preventDefault();
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar className={classes.headerContainer}>
+                    <Typography className={classes.title} variant="h6" noWrap>
+                        <Link className={classes.logo} href="#" onClick={preventDefault} color="inherit">
+                            LOGO
+                        </Link>
+                    </Typography>
+                    <Box className={classes.linkContainer}>
+                        <Typography className={classes.title} variant="h6" noWrap>
+                            <Link className={classes.logo} href="#" onClick={preventDefault} color="inherit">
+                                Link1
+                        </Link>
+                        </Typography>
+                        <Typography className={classes.title} variant="h6" noWrap>
+                            <Link className={classes.logo} href="#" onClick={preventDefault} color="inherit">
+                                Link2
+                        </Link>
+                        </Typography>
+                        <Typography className={classes.title} variant="h6" noWrap>
+                            <Link className={classes.logo} href="#" onClick={preventDefault} color="inherit">
+                                Link3
+                        </Link>
+                        </Typography>
+                        <Typography className={classes.title} variant="h6" noWrap>
+                            <Link className={classes.logo} href="#" onClick={preventDefault} color="inherit">
+                                Link4
+                        </Link>
+                        </Typography>
+                    </Box>
+                    <IconButton aria-label="show 17 new notifications" color="inherit">
+                        <Badge badgeContent={17} color="secondary">
+                            <ShoppingBasketIcon />
+                        </Badge>
+                    </IconButton>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase
+                            placeholder="Search…"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useRef } from 'react'
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+interface Props {
+    text: string;
+    // optional
+    ok?: boolean;
+    i?: number;
+    fn?: (bob: string) => string;
+    person: Person;
+}
+
+export const ListItem: React.FC<Props> = () => {
+    // Hooks
+    const [count, setCount] = useState<number | null>(5);
+    const inputRef = useRef<HTMLInputElement>(null);
+    const divRef = useRef<HTMLDivElement>(null);
+
+    return (
+        <div ref={divRef}>
+            <input ref={inputRef} />
+        </div>
+    )
+}
